04436b84a5d463d89f73894285ba58a7fcbfc126
Ilya Daraseliya <idarase@gmail.com>
2022-07-07 21:59:38 +0200

    add link t gpt 3

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -28,7 +28,15 @@ export default function Layout({ children }: React.PropsWithChildren<{}>) {
               d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
             />
           </svg>
-          All articles from The AI Paper are carefully crafted by GPT-3
+          All articles from The AI Paper are carefully crafted by{" "}
+          <a
+            className="text-gray-50 underline"
+            href="https://openai.com/api/"
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            GPT-3
+          </a>
         </div>
       </div>
       <div className="max-w-5xl w-full mx-auto bg-white border border-gray-200">
