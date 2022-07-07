2963bb92a8091b4b77c47247e52d5d0002428477
Ilya Daraseliya <idarase@gmail.com>
2022-07-07 20:34:17 +0200

    add banner

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,7 +11,26 @@ export default function Layout({ children }: React.PropsWithChildren<{}>) {
           </div>
         </div>
       </div>
-      <div className="max-w-4xl w-full mx-auto bg-white border-r border-l border-b border-gray-200 mb-4">
+      <div className="max-w-4xl w-full mx-auto mb-4 mt-4">
+        <div className="p-4 bg-yellow-500 rounded-md text-gray-50">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 inline-block mr-1"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+            />
+          </svg>
+          All articles from The AI Paper are carefully crafted by GPT-3
+        </div>
+      </div>
+      <div className="max-w-4xl w-full mx-auto bg-white border border-gray-200 mb-4">
         <main className="w-full">{children}</main>
       </div>
     </div>
