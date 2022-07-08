2328b8f3d7a54dcc109c628c791538c474560b59
Ilya Daraseliya <idarase@gmail.com>
2022-07-08 16:17:07 +0200

    move meta to _app

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,14 +5,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* <link
-            href="https://fonts.googleapis.com/css2?family=Gulzar&display=optional"
-            rel="stylesheet"
-          /> */}
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
           <link rel="icon" type="image/png" href="/favicon.png" />
         </Head>
         <body>
