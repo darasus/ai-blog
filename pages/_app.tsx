f82ac36ff04cec40263d48b5b2fabf0fb9684d79
Ilya Daraseliya <idarase@gmail.com>
2022-07-08 21:22:40 +0200

    add fathom analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,30 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import Head from "next/head";
+import * as Fathom from "fathom-client";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    Fathom.load("UYYOEIHH", {
+      includedDomains: ["www.theaipaper.com"],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Layout>
       <Head>
