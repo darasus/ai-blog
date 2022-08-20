ef9548a7d6d32b9649361a142816843194a8fa6d
Ilya Daraseliya <idarase@gmail.com>
2022-08-20 08:41:03 +0200

    clean up dependencies

--- scripts/generate-articles.ts
+++ scripts/generate-articles.ts
@@ -2,72 +2,109 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-import ora from "ora";
-import { AI } from "../lib/ai";
 import slugify from "slugify";
 import path from "node:path";
 import fs from "node:fs";
 import { MDXPost } from "../types/Post";
 import parseMD from "parse-md";
-import { readArticleFile } from "../utils/readFileSync";
+import { readArticleFile, readArticleImageFile } from "../utils/readFileSync";
 import { getArticleData } from "./shared/getArticleData";
 import { formatMarkdown } from "./shared/formatMarkdown";
-
-const spinner = ora("Start generating articles...");
+import { Writesonic } from "../lib/writesonic";
+import { allConcurrent } from "./shared/allConcurrent";
+import sharp from "sharp";
+import { Dalle } from "../lib/dalle";
 
 const postsDir = path.join(__dirname, "../content");
+const imagesDir = path.join(__dirname, "../public/articles");
 
 async function main() {
-  spinner.start();
-  const ai = new AI();
+  console.log("Generating articles...");
+  const ai = new Writesonic();
   const titlesAndCategories = getArticleData();
+  const promises = [];
 
   for (const [i, { title, category }] of titlesAndCategories.entries()) {
-    spinner.text = `Generating article (${i + 1} of ${
-      titlesAndCategories.length
-    }): ${title}`;
-    let post: Partial<MDXPost> = {};
-    const basename = slugify(title, { strict: true, lower: true });
-    // existing content
-    const originalSource = readArticleFile(basename + ".md", "utf8");
-    const { metadata, content } = parseMD(
-      typeof originalSource !== "string" ? "" : originalSource
-    ) as { metadata: MDXPost; content: string };
-    // new content
-    const { response } = await ai
-      .createCompletion(`Generate 1000 words article body titled "${title}"`)
-      .then((response) => ({
+    const promise = async () => {
+      console.log(`Generating article titled: ${title}`);
+      let post: Partial<MDXPost> = {};
+      const basename = slugify(title, { strict: true, lower: true });
+      // existing content
+      const originalSource = readArticleFile(basename + ".md");
+      const { metadata, content } = parseMD(
+        typeof originalSource !== "string" ? "" : originalSource
+      ) as { metadata: MDXPost; content: string };
+      // new content
+      const response = await ai.generateArticle({
         title,
-        response,
-      }));
+      });
 
-    if (content.trim() === response?.choices?.[0]?.text?.trim()) {
-      post.title = metadata.title;
-      post.createdAt = metadata.createdAt;
-      post.updatedAt = metadata.updatedAt;
-      post.category = metadata.category;
-      post.content = content;
-    } else {
-      if (metadata?.title && metadata?.createdAt) {
-        post.title = metadata?.title;
-        post.createdAt = metadata?.createdAt;
+      if (content.trim() === response.content.trim()) {
+        post.title = metadata.title;
+        post.createdAt = metadata.createdAt;
+        post.updatedAt = metadata.updatedAt;
+        post.category = metadata.category;
+        post.summary = metadata.summary;
+        post.intro = metadata.intro;
+        post.content = content;
+        if (!metadata.summary && response.summary) {
+          post.summary = response.summary;
+        }
       } else {
-        post.title = title;
-        post.createdAt = new Date();
+        if (metadata?.title && metadata?.createdAt) {
+          post.title = metadata?.title;
+          post.createdAt = metadata?.createdAt;
+        } else {
+          post.title = title;
+          post.createdAt = new Date();
+        }
+
+        post.updatedAt = new Date();
+        post.category = category;
+        post.content = response?.content;
+        post.summary = response?.summary;
+        post.intro = response?.intro;
       }
 
-      post.updatedAt = new Date();
-      post.category = category;
-      post.content = response?.choices?.[0].text;
-    }
+      const image = readArticleImageFile(basename + ".png");
 
-    const formattedPost = formatMarkdown(post as MDXPost);
-    const filePath = path.join(postsDir, `${basename}.md`);
-    fs.writeFileSync(filePath, formattedPost);
+      if (image) {
+        const img = sharp(image).resize(50);
+        const base64url = (await img.toBuffer()).toString("base64");
+        post.imageSrc = "/articles/" + basename + ".png";
+        post.imageSrcBase64 = "data:" + base64url + ";base64,";
+      } else {
+        const dalle = new Dalle();
+        const base64String = await dalle.generateImage(
+          `Intro image for article titled "${post.title}", pixel art`
+        );
+        if (base64String) {
+          fs.writeFileSync(
+            path.join(imagesDir, `${basename}.png`),
+            base64String,
+            "base64"
+          );
+          post.imageSrc = "/articles/" + basename + ".png";
+          const img = sharp(Buffer.from(base64String, "base64")).resize(50);
+          const imageSrcBase64 = (await img.toBuffer()).toString("base64");
+          post.imageSrcBase64 = "data:" + imageSrcBase64 + ";base64,";
+        } else {
+          post.imageSrc = "";
+          post.imageSrcBase64 = "";
+        }
+      }
+
+      const formattedPost = formatMarkdown(post as MDXPost);
+      const filePath = path.join(postsDir, `${basename}.md`);
+      fs.writeFileSync(filePath, formattedPost);
+    };
+
+    promises.push(promise);
   }
 
-  spinner.text = `Done generating ${titlesAndCategories.length} articles!`;
-  spinner.stopAndPersist();
+  await allConcurrent(1, promises);
+
+  console.log(`Done generating ${titlesAndCategories.length} articles!`);
   process.exit(0);
 }
 
