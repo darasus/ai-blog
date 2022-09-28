8bd1c6079e80920494fbcb7f67f6d661017a9cc6
Ilya Daraseliya <idarase@gmail.com>
2022-09-28 11:42:57 +0200

    remove use fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../components/Layout";
 import Head from "next/head";
-import * as Fathom from "fathom-client";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
@@ -10,27 +9,6 @@ import { theme } from "../theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
-
-  useEffect(() => {
-    function onRouteChangeComplete() {
-      Fathom.trackPageview();
-    }
-
-    if (process.env.NODE_ENV === "production") {
-      Fathom.load("UYYOEIHH", {
-        includedDomains: ["www.theaipaper.com"],
-        url: "https://events.theaipaper.com/script.js",
-      });
-
-      router.events.on("routeChangeComplete", onRouteChangeComplete);
-
-      return () => {
-        router.events.off("routeChangeComplete", onRouteChangeComplete);
-      };
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   return (
     <ChakraProvider theme={theme}>
