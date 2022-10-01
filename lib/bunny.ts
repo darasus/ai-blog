186eb14fa78034654d69872b952bcd5260612a70
Ilya Daraseliya <idarase@gmail.com>
2022-10-01 08:48:07 +0200

    move images to bunny

---
+++ lib/bunny.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import sharp from "sharp";
+import { readArticleImageFile } from "../node-utils/readArticleImageFile";
+import fs from "fs";
+import { imagesPath } from "../node-utils/paths";
+import fetch from "node-fetch";
+
+export async function uploadImage(basename: string) {
+  console.log("uploading " + basename);
+  //   const image = readArticleImageFile(basename + ".png");
+  const image = fs.readFileSync(`${imagesPath}/${basename}.png`);
+  if (!image) return null;
+  //   const img = sharp(image);
+  //   const base64url = image.buffer.toString();
+  //   const base64 = "data:image/png;base64," + base64url;
+  const base64 = "data:image/png;base64," + image.toString("base64");
+
+  return fetch(
+    `https://storage.bunnycdn.com/theaipaper/images/${basename}.png`,
+    {
+      method: "PUT",
+      headers: {
+        AccessKey: "4375ddb6-d5bd-49f3-8167f7215ace-58ef-4eb0",
+        "content-type": "application/octet-stream",
+      },
+      body: base64,
+    }
+  ).then(console.log);
+}
--- scripts/generate.ts
+++ scripts/generate.ts
@@ -3,23 +3,33 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import ora from "ora";
+import path from "path";
+import { uploadImage } from "../lib/bunny";
+import { imagesPath } from "../node-utils/paths";
 import { generateArticles } from "./generateArticles";
 import { generateSitemap } from "./generateSitemap";
 
 async function generate() {
-  let spinner = ora({
-    text: "Start generating...",
-    prefixText: "🚀",
-  }).start();
-  spinner.stopAndPersist();
-  spinner.start();
+  // let spinner = ora({
+  //   text: "Start generating...",
+  //   prefixText: "🚀",
+  // }).start();
+  // spinner.stopAndPersist();
+  // spinner.start();
 
-  await generateArticles(spinner);
-  await generateSitemap(spinner);
+  // await generateArticles(spinner);
+  // await generateSitemap(spinner);
 
-  spinner.prefixText = "✅";
-  spinner.text = `All done!`;
-  spinner.stopAndPersist();
+  await new Promise(async (resolve, reject) => {
+    await uploadImage(
+      "3-easy-exercises-you-can-do-in-your-home-that-will-make-you-lose-weight"
+    );
+    resolve(true);
+  });
+
+  // spinner.prefixText = "✅";
+  // spinner.text = `All done!`;
+  // spinner.stopAndPersist();
   process.exit(0);
 }
 
