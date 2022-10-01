a0c6642ca60d3c243b9fb4a5bfb3a46d41341637
Ilya Daraseliya <idarase@gmail.com>
2022-10-01 16:16:02 +0200

    saving progress

--- next.config.js
+++ next.config.js
@@ -12,6 +12,14 @@ const nextConfig = {
   images: {
     domains: ["theaipaper.b-cdn.net"],
   },
+  webpack(config, { dev, ...other }) {
+    if (!dev) {
+      // https://formatjs.io/docs/guides/advanced-usage#react-intl-without-parser-40-smaller
+      config.resolve.alias["@formatjs/icu-messageformat-parser"] =
+        "@formatjs/icu-messageformat-parser/no-parser";
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
