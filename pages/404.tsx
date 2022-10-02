482f0b732abdbc9954c3dd10204e944380e3da65
Ilya Daraseliya <idarase@gmail.com>
2022-10-02 08:02:17 +0200

    add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,13 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { NoSymbolIcon } from "@heroicons/react/24/outline";
+
+export default function NotFound() {
+  return (
+    <Flex direction={"column"} p={4} alignItems="center">
+      <Box mb={4}>
+        <NoSymbolIcon width={100} height={100} />
+      </Box>
+      <Text>Opps... Something went wrong.</Text>
+    </Flex>
+  );
+}
