c23c3ea17cf749979456565c5c876511419b9785
Ilya Daraseliya <idarase@gmail.com>
2022-10-18 07:53:03 +0200

    add lazy to post excerpt

--- components/Post/PostExcerpt.tsx
+++ components/Post/PostExcerpt.tsx
@@ -28,6 +28,7 @@ export function PostExcerpt({ post }: Props) {
             alt={title}
             placeholder="blur"
             fill
+            loading="lazy"
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           />
         </AspectRatio>
