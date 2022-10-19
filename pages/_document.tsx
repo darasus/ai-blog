bc1104b883e418daad00d66b7558fe9c8b3057ad
Ilya Daraseliya <idarase@gmail.com>
2022-10-19 15:18:26 +0200

    remove unneccesary ga script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,15 +10,6 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          `,
-            }}
-          />
         </body>
       </Html>
     )
