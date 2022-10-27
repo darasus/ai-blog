5d3bcdbfc0a081cc97deb4700a090b1ea272f9a5
Ilya Daraseliya <idarase@gmail.com>
2022-10-27 22:20:43 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false,
-  experimental: {},
   i18n: {
     locales: ['en', 'es'],
     defaultLocale: 'en',
