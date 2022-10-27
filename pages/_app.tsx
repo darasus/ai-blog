4185dc022a206d9c45fe6625bc5d5ac1ea5194d1
Ilya Daraseliya <idarase@gmail.com>
2022-10-27 21:13:52 +0200

    add vercel/analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,51 +1,20 @@
 import '../styles/globals.css'
+
 import type { AppProps } from 'next/app'
 import { Layout } from '../components/Layout'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import Script from 'next/script'
-import { useEffect } from 'react'
-import * as gtag from '../lib/gtag'
 import { IntlProvider } from 'react-intl'
+import { Analytics } from '@vercel/analytics/react'
 
 function MyApp({
   Component,
   pageProps,
 }: AppProps<{ intlMessages: Record<string, string> }>) {
-  const { events, locale, defaultLocale } = useRouter()
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      gtag.pageview(url)
-    }
-
-    events.on('routeChangeComplete', handleRouteChange)
-    events.on('hashChangeComplete', handleRouteChange)
-    return () => {
-      events.off('routeChangeComplete', handleRouteChange)
-      events.off('hashChangeComplete', handleRouteChange)
-    }
-  }, [events])
+  const { locale, defaultLocale } = useRouter()
 
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
       <IntlProvider
         locale={locale as string}
         defaultLocale={defaultLocale}
@@ -61,6 +30,7 @@ function MyApp({
           <Component {...pageProps} />
         </Layout>
       </IntlProvider>
+      <Analytics />
     </>
   )
 }
